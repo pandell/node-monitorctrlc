9152e8a85391c8fc4fc8b1a106d4e533a3c0125f
Milan Gardian <mgardian@gmail.com>
2014-06-10 17:58:32 -0600

    Add initial implementation

---
+++ index.js
@@ -0,0 +1,24 @@
+/*jslint node: true, vars: true */
+
+"use strict";
+
+var log = require("gulp/node_modules/gulp-util").log;
+var chalk = require("gulp/node_modules/chalk");
+
+function defaultCtrlCHandler() {
+    log("'" + chalk.cyan('^C') + "'" + ', exiting');
+    process.exit();
+}
+
+module.exports = function monitorCtrlC(cb) {
+    var stdin = process.stdin;
+    if (stdin && stdin.isTTY) {
+        if (typeof cb !== 'function') { cb = defaultCtrlCHandler; }
+        stdin.setRawMode(true);
+        stdin.on('data', function monitorCtrlCOnData(data) {
+            if (data.length === 1 && data[0] === 0x03) { // Ctrl+C
+                return cb();
+            }
+        });
+    }
+};
