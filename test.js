a157802dd183b6dce3e421d8ccd694bb61c1abee
Milan Gardian <mgardian@gmail.com>
2014-06-11 08:08:15 -0600

    Implement "hijackSystemCalls" as a higher-order function

--- test.js
+++ test.js
@@ -11,65 +11,62 @@ var monitorCtrlC = require("./");
 
 // We can't use mocha's "beforeEach" and "afterEach" because we're hijacking
 // system behaviour and need to restore the original one as soon as possible
-function hijackSystemCalls(consoleBuffer, cb) {
-    var emitter = new events.EventEmitter();
+function hijackSystemCalls(cb) {
+    return function hijackSystemCallsRun() {
+        var consoleBuffer = [];
+        var emitter = new events.EventEmitter();
 
-    var originalOn = process.stdin.on;
-    process.stdin.on = emitter.on.bind(emitter);
+        var originalOn = process.stdin.on;
+        process.stdin.on = emitter.on.bind(emitter);
 
-    var originalEmit = process.stdin.emit;
-    process.stdin.emit = emitter.emit.bind(emitter);
+        var originalEmit = process.stdin.emit;
+        process.stdin.emit = emitter.emit.bind(emitter);
 
-    var originalIsTTY = process.stdin.isTTY;
-    process.stdin.isTTY = true;
+        var originalIsTTY = process.stdin.isTTY;
+        process.stdin.isTTY = true;
 
-    var originalSetRawMode = process.stdin.setRawMode;
-    process.stdin.setRawMode = function () { consoleBuffer.push("setRawMode"); };
+        var originalSetRawMode = process.stdin.setRawMode;
+        process.stdin.setRawMode = function () { consoleBuffer.push("setRawMode"); };
 
-    var originalProcessExit = process.exit;
-    process.exit = function () { consoleBuffer.push("exit"); };
+        var originalProcessExit = process.exit;
+        process.exit = function () { consoleBuffer.push("exit"); };
 
-    var originalConsoleLog = console.log;
-    console.log = function () { consoleBuffer.push(Array.prototype.slice.call(arguments, 0)); };
+        var originalConsoleLog = console.log;
+        console.log = function () { consoleBuffer.push(Array.prototype.slice.call(arguments, 0)); };
 
-    try {
-        cb();
-    } finally {
-        console.log = originalConsoleLog;
-        process.exit = originalProcessExit;
-        process.stdin.setRawMode = originalSetRawMode;
-        process.stdin.isTTY = originalIsTTY;
-        process.stdin.emit = originalEmit;
-        process.stdin.on = originalOn;
-    }
+        try {
+            return cb(consoleBuffer);
+        } finally {
+            console.log = originalConsoleLog;
+            process.exit = originalProcessExit;
+            process.stdin.setRawMode = originalSetRawMode;
+            process.stdin.isTTY = originalIsTTY;
+            process.stdin.emit = originalEmit;
+            process.stdin.on = originalOn;
+        }
+    };
 }
 
 
 describe("monitorCtrlC()", function () {
 
-    it("should use default handler", function () {
-        var consoleBuffer = [];
-        hijackSystemCalls(consoleBuffer, function () {
-            monitorCtrlC();
-            process.stdin.emit("data", new Buffer("\u0003")); // fake ^C
+    it("should use default handler", hijackSystemCalls(function (consoleBuffer) {
+        monitorCtrlC();
+        process.stdin.emit("data", new Buffer("\u0003")); // fake ^C
 
-            assert.strictEqual(3, consoleBuffer.length);
-            assert.strictEqual("setRawMode", consoleBuffer[0]);
-            assert(consoleBuffer[1].join(" ").indexOf("exiting") > 0);
-            assert.strictEqual("exit", consoleBuffer[2]);
-        });
-    });
+        assert.strictEqual(3, consoleBuffer.length);
+        assert.strictEqual("setRawMode", consoleBuffer[0]);
+        assert(consoleBuffer[1].join(" ").indexOf("exiting") > 0);
+        assert.strictEqual("exit", consoleBuffer[2]);
+    }));
 
-    it("should use specified handler", function () {
-        var consoleBuffer = [];
-        hijackSystemCalls(consoleBuffer, function () {
-            monitorCtrlC(function () { consoleBuffer.push("custom"); });
-            process.stdin.emit("data", new Buffer("\u0003")); // fake ^C
+    it("should use specified handler", hijackSystemCalls(function (consoleBuffer) {
+        monitorCtrlC(function () { consoleBuffer.push("custom"); });
+        process.stdin.emit("data", new Buffer("\u0003")); // fake ^C
 
-            assert.strictEqual(2, consoleBuffer.length);
-            assert.strictEqual("setRawMode", consoleBuffer[0]);
-            assert.strictEqual("custom", consoleBuffer[1]);
-        });
-    });
+        assert.strictEqual(2, consoleBuffer.length);
+        assert.strictEqual("setRawMode", consoleBuffer[0]);
+        assert.strictEqual("custom", consoleBuffer[1]);
+    }));
 
 });
